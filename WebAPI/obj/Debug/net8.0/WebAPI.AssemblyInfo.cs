7aba88295bf6c724af2d427524ad30ac44c03225
hoanhngjp <hoanhnghiep2704@gmail.com>
2025-10-15 22:14:03 +0700

    test

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d66b5e72e51b9420b69a69a9a1953e1ef36907")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea1c216cadcdb8c689b97c5897151178184c679")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
